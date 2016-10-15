b52e23a138d7b19016f4395a28ed1a5950176bcb
tttra <tothutra@gmail.com>
2016-10-14 23:40:32 -0400

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -3,3 +3,19 @@ describe('example test', function() {
         expect('foo').toBe('foo');
     });
 });
+
+describe('MainController', function() {
+  beforeEach(module('app'));
+  
+  var $controller;
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  it('should have Steve Jobs name', function() {
+    var $scope = {};
+    var controller = $controller('MainController', { $scope: $scope});
+    expect(controller.name).toEqual('Steve Jobs')
+  })
+});
